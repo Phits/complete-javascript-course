c106c10c4db3eb5e079425fc0fa5576873c01097
phits <john@fitz.com>
2021-02-16 09:42:10 -0500

    ES6 default values

--- 10-Functions copy/starter/script.js
+++ 10-Functions copy/starter/script.js
@@ -1,1 +1,28 @@
 'use strict';
+
+const bookings = [];
+
+// ES 6 Default values
+const createBooking = function(
+    flightNum, 
+    numPassengers = 1, 
+    price = 199 * numPassengers
+    ) {
+
+    // ES 5 Default Values
+    // numPassengers = numPassengers || 1;
+    // price = price || 199;
+   
+    const booking = {
+        flightNum,
+        numPassengers,
+        price,
+    }
+    console.log(booking);
+    bookings.push(booking);
+}
+
+createBooking('LH123', 2,);
+createBooking('LH123', 2, 800);
+createBooking('LH123', 2);
+createBooking('LH123', undefined, 1000);
