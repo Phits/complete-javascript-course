7716af08655d170ff88246e00ec411202b8cf0e3
phits <john@fitz.com>
2021-03-15 20:34:14 -0400

    Error catching

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -4,7 +4,7 @@ const btn = document.querySelector(".btn-country");
 const countriesContainer = document.querySelector(".countries");
 
 const renderCountry = function (data, className = "") {
-    const html = `<article class="country ${className}">
+  const html = `<article class="country ${className}">
         <img class="country__img" src="${data.flag}">
           <div class="country__data">
             <h3 class="country__name">${data.name}</h3>
@@ -13,16 +13,18 @@ const renderCountry = function (data, className = "") {
               +data.population / 10000000
             ).toFixed(1)}M people</p>
             <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-            <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+            <p class="country__row"><span>💰</span>${
+              data.currencies[0].name
+            }</p>
           </div>
         </article>`;
-  
-    countriesContainer.insertAdjacentHTML("beforeend", html);
-  };
-  
-  const renderError = function(msg) {
-      countriesContainer.insertAdjacentText('beforeend', msg);
-  }
+
+  countriesContainer.insertAdjacentHTML("beforeend", html);
+};
+
+const renderError = function (msg) {
+  countriesContainer.insertAdjacentText("beforeend", msg);
+};
 
 ///////////////////////////////////////
 
@@ -127,34 +129,93 @@ const renderCountry = function (data, className = "") {
 // };
 
 // Arrow functions & Chaining Promises
+
+// Helper Function
+const getJSON = function (url, errorMsg = "Somthing went wrong.") {
+  return fetch(url).then((response) => {
+    if (!response.ok) throw new Error(`${errorMsg} ${response.status}`);
+
+    return response.json();
+  });
+};
+
+// const getCountryData = function (country) {
+//   // Country 1
+//   const request = fetch(`https://restcountries.eu/rest/v2/name/${country}`)
+//     .then((response) => {
+//       if (!response.ok) throw new Error(`Country not found ${response.status}`);
+
+//       return response.json();
+//     })
+//     .then((data) => {
+//       renderCountry(data[0]);
+//       //   const neightbour = data[0].borders[0];'
+//       const neightbour = "sadfafs";
+//       if (!neightbour) return;
+
+//       // Country 2
+//       return fetch(`https://restcountries.eu/rest/v2/alpha/${neightbour}`);
+//     })
+//     .then((response) => {
+//       if (!response.ok) throw new Error(`Country not found ${response.status}`);
+
+//       return response.json();
+//     })
+//     .then((data) => renderCountry(data, "neighbour"))
+//     .catch((err) => {
+//       console.error("Error is ", err);
+//       renderError(`Something went wrong ${err.message}. Try again!`);
+//     })
+//     .finally(() => {
+//       countriesContainer.style.opacity = 1;
+//     });
+// };
+
+// btn.addEventListener("click", function () {
+//   getCountryData("ireland");
+// });
+
 const getCountryData = function (country) {
   // Country 1
-  const request = fetch(`https://restcountries.eu/rest/v2/name/${country}`)
-    .then((response) => response.json())
+  getJSON(
+    `https://restcountries.eu/rest/v2/name/${country}`,
+    "Country not found"
+  )
     .then((data) => {
       renderCountry(data[0]);
       const neightbour = data[0].borders[0];
 
-      if (!neightbour) return;
+      if (!neightbour) throw new Error('No neighbour found');
 
       // Country 2
-      return fetch(`https://restcountries.eu/rest/v2/alpha/${neightbour}`);
+      return getJSON(
+        `https://restcountries.eu/rest/v2/alpha/${neightbour}`,
+        "Country not found"
+      );
     })
-    .then((response) => response.json())
     .then((data) => renderCountry(data, "neighbour"))
     .catch((err) => {
       console.error("Error is ", err);
-      renderError(`Something went wrong ${err.message}. Try again!`);
+      renderError(`Something went wrong. ${err.message}. Try again!`);
     })
     .finally(() => {
-        countriesContainer.style.opacity = 1;
-    })
+      countriesContainer.style.opacity = 1;
+    });
 };
 
 btn.addEventListener("click", function () {
   getCountryData("ireland");
 });
 
+getCountryData("australia");
 
-getCountryData("faasdfasdfasdf");
+////////////////////////////////////////////
+// Challenge Geocoding 
+
+
+
+
+
+
+
 
